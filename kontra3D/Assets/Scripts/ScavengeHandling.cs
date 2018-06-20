59693117e223799cb7471d955198d32aadbec2a5
Holdypopn <dominik.n8@googlemail.com>
2018-06-20 22:12:34 +0200

    Add ScavengeHandlingScene with some basic UI and empty ScavengeHandling.cs

---
+++ kontra3D/Assets/Scripts/ScavengeHandling.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScavengeHandling : MonoBehaviour {
+
+    Player player;
+
+	// Use this for initialization
+	void Start () {
+        player = Player.playerInstance;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
