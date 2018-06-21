2c56e42d237497d9034968393b4075a481a47618
Holdypopn <dominik.n8@googlemail.com>
2018-06-21 08:35:11 +0200

    Add first draft of getRandomItem method in ScavengeHandling.cs

--- kontra3D/Assets/Scripts/ScavengeHandling.cs
+++ kontra3D/Assets/Scripts/ScavengeHandling.cs
@@ -1,18 +1,39 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ScavengeHandling : MonoBehaviour {
 
-    Player player;
+    	Player player;
+	Inventory inventory;
+	List<InventoryItem_Base> itemList = inventoryInstance.availableItems;
 
 	// Use this for initialization
-	void Start () {
-        player = Player.playerInstance;
+	void Start ()
+	{
+        	player = Player.playerInstance;
+		inventory = Inventory.inventoryInstance;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+	
+	void getRandomItem()
+	{
+		int totalRarity = itemList.Sum(x => x.Rarity);
+		Random r = new Random();
+		var randomNumber = r.NextDouble() * totalRarity;
+
+		double totalSoFar = 0;
+		foreach (var item in itemList)
+		{
+			totalSoFar += item.Rarity;
+			if (totalSoFar > randomNumber)
+			{
+			    return item;
+			}
+		}
 	}
 }
