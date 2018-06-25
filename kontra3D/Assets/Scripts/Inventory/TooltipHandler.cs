2be1ee6f84a5332e7451c8f6f819be839c130634
MichiUff <beer_michael95@web.de>
2018-06-25 18:27:13 +0200

    Do not display hover menu on empty items

---
+++ kontra3D/Assets/Scripts/Inventory/TooltipHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TooltipHandler : MonoBehaviour {
+    public GameObject Hovermenu;
+
+	// Use this for initialization
+	public void SetActive () {
+        if(Hovermenu.transform.GetComponentInChildren<Text>().text.Trim() != "")
+            Hovermenu.SetActive(true);
+	}
+
+    // Update is called once per frame
+    public void SetInactive () {
+        Hovermenu.SetActive(false);
+    }
+}
